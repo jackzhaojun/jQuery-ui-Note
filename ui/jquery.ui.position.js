84d4857386fe7bf51a12bd1b8ca60a9302936972
jackzhaojun <zhaojunbest@gmail.com>
2014-07-29 11:11:30 +0800

    position详解

--- ui/jquery.ui.position.js
+++ ui/jquery.ui.position.js
@@ -279,7 +279,7 @@ $.fn.position = function( options ) {
 					feedback.important = "vertical";
 				}
 				options.using.call( this, props, feedback );
-			};
+        };
 		}
 
 		elem.offset( $.extend( position, { using: using } ) );
